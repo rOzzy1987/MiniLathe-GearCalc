48c6e57818eca1f86cf8caa14b03b32bb38b6bef
rOzzy1987 <rozovits.mihaly@gmail.com>
2023-04-14 12:46:50 +0200

    capacitor added

---
+++ android/app/src/main/java/com/rozzy87/gearcalc/MainActivity.java
@@ -0,0 +1,10 @@
+package com.rozzy87.gearcalc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
